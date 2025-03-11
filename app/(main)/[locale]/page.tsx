1e41e4ce5e687acb776ad7448f963a3fb54b5783
Jewel Rana <jewelranashepon@gmail.com>
2025-03-11 11:42:04 +0600

    Steps section link added

--- app/(main)/[locale]/page.tsx
+++ app/(main)/[locale]/page.tsx
@@ -3,10 +3,16 @@ import GetInTouch from "../../components/GetInTouch";
 import Image from "next/image";
 import { FaAward } from "react-icons/fa";
 import BlogCard from "../../components/BlogCard";
-import { CalendarCheck, Map, MapPinned, Stethoscope, Workflow } from "lucide-react";
-import { LineChart } from 'lucide-react';
-import { Book } from 'lucide-react';
-import { CalendarDays } from 'lucide-react';
+import {
+  CalendarCheck,
+  Map,
+  MapPinned,
+  Stethoscope,
+  Workflow,
+} from "lucide-react";
+import { LineChart } from "lucide-react";
+import { Book } from "lucide-react";
+import { CalendarDays } from "lucide-react";
 import { FaBook, FaFlag, FaCompass } from "react-icons/fa";
 import MockTestSection from "@/app/components/MockTestSection";
 import VideoSection from "@/app/components/Video";
@@ -18,8 +24,6 @@ export async function generateMetadata(): Promise<Metadata> {
   const t = await getTranslations();
   const title = t("home.metadata.title");
   const description = t("home.metadata.description");
- 
-  
 
   return {
     title: title,
@@ -31,13 +35,13 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-const HomePage: React.FC = async() => {
+const HomePage: React.FC = async () => {
   const t = await getTranslations();
   const steps = t.raw("home.Steps.StepSection");
   const promises = t.raw("home.promiseCards.promiseCardContent");
   const states = t.raw("home.StatesSection.StateContent");
   const courses = t.raw("home.Courses.CourseCards");
-  const blogs = t.raw("home.BlogSection.blogContent")
+  const blogs = t.raw("home.BlogSection.blogContent");
 
   const icons: JSX.Element[] = [
     <CalendarCheck key="calendar" size={48} strokeWidth={1.5} />,
@@ -46,17 +50,17 @@ const HomePage: React.FC = async() => {
     <Workflow key="workflow" size={48} strokeWidth={1.5} />,
   ];
   const promiseIcons: JSX.Element[] = [
-  <Map size={48} strokeWidth={1.5} />,
-  <LineChart size={48} strokeWidth={1.5} />,
-  <Book size={48} strokeWidth={1.5} />,
-  <CalendarDays size={48} strokeWidth={1.5} />,
+    <Map size={48} strokeWidth={1.5} />,
+    <LineChart size={48} strokeWidth={1.5} />,
+    <Book size={48} strokeWidth={1.5} />,
+    <CalendarDays size={48} strokeWidth={1.5} />,
   ];
 
   const statesIcon: JSX.Element[] = [
     <FaBook className="text-6xl text-sky-800" />,
     <FaFlag className="text-6xl text-sky-800" />,
     <FaCompass className="text-6xl text-sky-800" />,
-  ]
+  ];
 
   return (
     <div className="bg-gray-100">
@@ -96,18 +100,28 @@ const HomePage: React.FC = async() => {
       {/* Steps Section */}
       <section className="py-12 bg-gray-100">
         <div className="max-w-6xl mx-auto px-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">            
-            {steps.map((StepSection,index) => (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            {steps.map((StepSection, index) => (
               <div
                 key={index}
                 className="flex flex-col items-center text-center p-6 bg-teal-600 text-white rounded-2xl shadow-lg transition-transform hover:scale-105"
               >
                 <div className="bg-white bg-opacity-20 p-4 rounded-full mb-4">
-                {icons[index]}
+                  {icons[index]}
                 </div>
-                <h3 className="text-xl font-semibold mb-2">{StepSection.title} </h3>
+                <h3 className="text-xl font-semibold mb-2">
+                  {StepSection.title}{" "}
+                </h3>
                 <p className="text-sm">{StepSection.description}</p>
-                <div className="mt-4 text-white text-lg">→</div>
+                <div className="mt-4 text-white text-lg">
+                  {/* Adding Link here */}
+                  <Link href={StepSection.link}>
+                    <span className="mt-4 text-lg hover:text-teal-400 transition duration-300">
+                      Learn More
+                    </span>
+                  </Link>
+                  →
+                </div>
               </div>
             ))}
           </div>
@@ -144,16 +158,18 @@ const HomePage: React.FC = async() => {
                   <h4 className="text-2xl font-bold text-gray-800">
                     {t("home.SomethingAboutUs.Yrs")}
                   </h4>
-                  <p className="text-gray-600">{t("home.SomethingAboutUs.PE")}</p>
+                  <p className="text-gray-600">
+                    {t("home.SomethingAboutUs.PE")}
+                  </p>
                 </div>
               </div>
 
               <div className="flex space-x-4">
                 <button className="bg-yellow-600 text-white py-2 px-6 rounded-md shadow hover:bg-yellow-700 transition duration-300">
-                {t("home.SomethingAboutUs.BNBTN")}
+                  {t("home.SomethingAboutUs.BNBTN")}
                 </button>
                 <button className="border-2 border-gray-800 text-gray-800 py-2 px-6 rounded-md hover:bg-gray-800 hover:text-white transition duration-300">
-                {t("home.SomethingAboutUs.CNBTN")}
+                  {t("home.SomethingAboutUs.CNBTN")}
                 </button>
               </div>
             </div>
@@ -164,33 +180,33 @@ const HomePage: React.FC = async() => {
       {/* Stats Section */}
       <section className="text-center py-16">
         <h3 className="text-lg font-semibold text-gray-900">
-        {t("home.StatSection.find")}
+          {t("home.StatSection.find")}
         </h3>
         <h2 className="text-3xl font-bold text-blue-800 mt-2">
-        {t("home.StatSection.TopographicalTitle")}
+          {t("home.StatSection.TopographicalTitle")}
         </h2>
         <p className="mt-4 max-w-2xl mx-auto text-gray-700">
-        {t("home.StatSection.TopographicalDescription")}
+          {t("home.StatSection.TopographicalDescription")}
         </p>
         <div className="mt-6">
-        <Link
-          href="/exam"
-          className="border-2 border-blue-800 text-blue-800 px-6 py-3 rounded-lg font-semibold hover:bg-blue-800 hover:text-white transition inline-block"
-        >
-           {t("home.StatSection.StartTest")}
-        </Link>
-      </div>
+          <Link
+            href="/exam"
+            className="border-2 border-blue-800 text-blue-800 px-6 py-3 rounded-lg font-semibold hover:bg-blue-800 hover:text-white transition inline-block"
+          >
+            {t("home.StatSection.StartTest")}
+          </Link>
+        </div>
       </section>
 
-      
-
-      <MockTestSection/>
+      <MockTestSection />
 
       {/* Promise Section */}
       <section className="text-center py-12 bg-white">
-        <h2 className="text-3xl font-semibold mb-4">{t('home.promiseSection.promiseTitle')}</h2>
+        <h2 className="text-3xl font-semibold mb-4">
+          {t("home.promiseSection.promiseTitle")}
+        </h2>
         <p className="text-lg text-gray-600 max-w-2xl mx-auto mb-10">
-        {t('home.promiseSection.promiseDescription')}
+          {t("home.promiseSection.promiseDescription")}
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-6">
           {promises.map((promiseCardContent, index) => (
@@ -201,7 +217,9 @@ const HomePage: React.FC = async() => {
               <div className="bg-gradient-to-r from-red-500 to-yellow-500 p-4 rounded-full mb-4">
                 {promiseIcons[index]}
               </div>
-              <h3 className="text-xl font-semibold mb-2">{promiseCardContent.title}</h3>
+              <h3 className="text-xl font-semibold mb-2">
+                {promiseCardContent.title}
+              </h3>
               <p className="text-gray-600">{promiseCardContent.description}</p>
             </div>
           ))}
@@ -214,7 +232,9 @@ const HomePage: React.FC = async() => {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
             {states.map((StateContent, index) => (
               <div key={index} className="text-slate-800">
-                <div className="flex justify-center mb-4">{statesIcon[index]}</div>
+                <div className="flex justify-center mb-4">
+                  {statesIcon[index]}
+                </div>
                 <h3 className="text-4xl font-bold">{StateContent.value}</h3>
                 <hr className="my-4 border-t-2 border-gray-400 w-1/2 mx-auto" />
                 <p className="text-lg font-semibold">{StateContent.label}</p>
@@ -230,13 +250,13 @@ const HomePage: React.FC = async() => {
           {/* Section Title */}
           <div className="text-center mb-10">
             <h3 className="text-purple-600 font-semibold text-lg uppercase">
-            {t('home.ourCourseSection.CourseTitle')}
+              {t("home.ourCourseSection.CourseTitle")}
             </h3>
             <h2 className="text-3xl font-bold text-gray-800">
-            {t('home.ourCourseSection.CoursePlanTitle')}
+              {t("home.ourCourseSection.CoursePlanTitle")}
             </h2>
             <p className="text-gray-600 mt-2">
-            {t('home.ourCourseSection.CourseShortDes')}
+              {t("home.ourCourseSection.CourseShortDes")}
             </p>
           </div>
 
@@ -275,18 +295,19 @@ const HomePage: React.FC = async() => {
                 </div>
 
                 <button className="bg-purple-600 text-white rounded-md py-2 px-4 w-full hover:bg-purple-700 transition duration-300">
-                {t("home.ourCourseSection.CP")}
+                  {t("home.ourCourseSection.CP")}
                 </button>
 
                 <ul className="text-left mt-6 space-y-2">
-        {/* Assuming `features` array is defined elsewhere in your code */}
-        {Courses.features && Courses.features.map((feature, idx) => (
-          <li key={idx} className="text-gray-600 flex items-start">
-            <span className="text-green-600 font-bold mr-2">✔</span>
-            {feature}
-          </li>
-        ))}
-      </ul>
+                  {/* Assuming `features` array is defined elsewhere in your code */}
+                  {Courses.features &&
+                    Courses.features.map((feature, idx) => (
+                      <li key={idx} className="text-gray-600 flex items-start">
+                        <span className="text-green-600 font-bold mr-2">✔</span>
+                        {feature}
+                      </li>
+                    ))}
+                </ul>
 
                 {/* <Link
                   href={courses.link}
@@ -301,7 +322,7 @@ const HomePage: React.FC = async() => {
       </section>
 
       {/*Video Section */}
-      <VideoSection/>
+      <VideoSection />
 
       {/* Our Recent Blogs Section */}
       <section className="py-16 bg-gray-50">
@@ -309,13 +330,13 @@ const HomePage: React.FC = async() => {
           {/* Section Title */}
           <div className="text-center mb-10">
             <h3 className="text-green-600 font-semibold text-lg uppercase">
-             {t("home.BlogSectionHeader.header")}
+              {t("home.BlogSectionHeader.header")}
             </h3>
             <h2 className="text-3xl font-bold text-gray-800">
-            {t("home.BlogSectionHeader.shortTitle")}
+              {t("home.BlogSectionHeader.shortTitle")}
             </h2>
             <p className="text-gray-600 mt-2">
-            {t("home.BlogSectionHeader.blogHeaderDes")}
+              {t("home.BlogSectionHeader.blogHeaderDes")}
             </p>
           </div>
 
